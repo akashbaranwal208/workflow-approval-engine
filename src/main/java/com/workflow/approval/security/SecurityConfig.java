ab4ed03d549d64e0f18d1045e096fd5014120886
Akash Baranwal <akashbaranwal208@gmail.com>
2026-02-19 19:46:15 +0530

    Fix SecurityConfig file

--- src/main/java/com/workflow/approval/security/SecurityConfig.java
+++ src/main/java/com/workflow/approval/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.workflow.approval.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,10 +20,8 @@ public class SecurityConfig {
         http
                 .csrf(csrf->csrf.disable())
                 .authorizeHttpRequests(auth->auth
-                        .requestMatchers("/api/auth/**",
-                        "/v3/api-docs/**",
-                        "/swagger-ui/**",
-                        "/swagger-ui.html").permitAll()
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(
