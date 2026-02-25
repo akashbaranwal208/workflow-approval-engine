728dab9816345da1fb0c8658b57d33ae405d10e7
Akash Baranwal <akashbaranwal208@gmail.com>
2026-02-25 14:22:02 +0530

    Fix cors for vercel

--- src/main/java/com/workflow/approval/security/SecurityConfig.java
+++ src/main/java/com/workflow/approval/security/SecurityConfig.java
@@ -8,8 +8,12 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.net.http.HttpClient;
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -32,5 +36,23 @@ public class SecurityConfig {
 
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://workflow-frontend-6ry967rig-akashbaranwal208s-projects.vercel.app/"
+        ));
+
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
